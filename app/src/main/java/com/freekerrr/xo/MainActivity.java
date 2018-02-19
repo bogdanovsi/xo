3c03ce001191416b7509729f2fb8e7028a8c9b72
freekerrr <bogdanov.s.i@rambler.ru>
2018-02-19 16:40:04 +0300

    add inner XOManager add Point class delete CanvasXO

--- app/src/main/java/com/freekerrr/xo/MainActivity.java
+++ app/src/main/java/com/freekerrr/xo/MainActivity.java
@@ -1,21 +1,30 @@
 package com.freekerrr.xo;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.DrawableWrapper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    DrawView drawView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawView = (DrawView) findViewById(R.id.drawView);
+        //  drawView.drawTable(900,900);
     }
 
-
+    public void clearCanvas(View view) {
+        drawView.clearCanvas();
+    }
 }
